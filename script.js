d5db8c7f74d3a8a4899a5e4fc7748ec51e111b0e
Falak Ejaz <157350415+Falakejaz786@users.noreply.github.com>
2025-04-27 20:45:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// JavaScript for portfolio interactions
+
+document.getElementById('contactBtn').addEventListener('click', function() {
+    alert('Thanks for reaching out! I will get back to you soon.');
+});
